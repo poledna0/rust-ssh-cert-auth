2e0a2de21e18c9f581b2185d0f1799e6493eb013
poledna0 <henriquepoledna@proton.me>
2025-11-07 11:03:06 -0300

    interface visual pronta

---
+++ PAN-SS/src/client.rs
@@ -0,0 +1,53 @@
+use std::{io::{self, Write}, u8};
+
+pub fn interface(){
+    let mut buffer: String = String::new();
+
+    println!("---------------------------------------------------------");
+    print!("Escolha uma opção:\n
+    (1) - fazer login;\n
+    (2) - criar conta;\n
+    (3) - acessar servidor ssh ( é necessario já ter feito login );\n
+
+->>> ");
+    io::stdout().flush().expect("Falha ao dar flush no stdout");
+
+    std::io::stdin().read_line(&mut buffer).expect("erro a ler a linha1");
+
+
+    let entrada = buffer.trim().parse::<u8>();
+
+    // um try em rust :o 
+    match entrada {
+
+
+        // depois de saber se deu certo e a pessoa colocou um numero e nao uma coisa que não pode ser convertida
+        // temos q tratar as opcoes para qual func vai ser levada
+        
+        Ok(valor_convertido) => {
+
+
+            match valor_convertido {
+                1 => {
+                    println!("1");
+                }
+
+                2 => {
+                        println!("2");
+                }
+
+                3 => {
+                        println!("3");
+                }
+                _=>{println!("Valor invalido")}
+            }
+        }
+        Err(e) => {
+            // aqui 'e' é um std::num::ParseIntError
+            eprintln!("Erro ao converter o valor para u8: {}", e);
+        }
+    }
+
+
+
+}
---
+++ PAN-SS/src/main.rs
@@ -0,0 +1,6 @@
+mod client;
+
+fn main() {
+    // chamando a interface que possui as opcoes de input para escolher oq o usuario deseja fazer
+    client::interface();
+}
